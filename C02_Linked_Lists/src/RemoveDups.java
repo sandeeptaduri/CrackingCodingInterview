dd68b0fd4bcb4f69e79dead4a3bd80b4e44446de
sandeeptaduri <sandeeptaduri@192.168.0.106>
2018-10-01 23:26:44 +0530

    using ListIterator

--- C02_Linked_Lists/src/RemoveDups.java
+++ C02_Linked_Lists/src/RemoveDups.java
@@ -1,5 +1,8 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
 import java.util.Scanner;
 
 public class RemoveDups {
@@ -7,7 +10,7 @@ public class RemoveDups {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		List<Integer> input = new ArrayList<Integer>();
+		List<Integer> input = new LinkedList<Integer>();
 		while(scan.hasNextInt()) {
 			input.add(scan.nextInt());
 			//System.out.println(input.size());
@@ -22,9 +25,29 @@ public class RemoveDups {
 
 	private static List<Integer> cleanDups(List<Integer> input) {
 		// TODO Auto-generated method stub
+		Map<Integer, Boolean> temp = new HashMap<Integer, Boolean>();
+		
+		ListIterator<Integer> listIter = input.listIterator();
+		int tempValue;
+		
+		while(listIter.hasNext()) {
+			tempValue=listIter.next();
+			
+			if(temp.containsKey(tempValue)) {
+				listIter.remove();
+				System.out.println(""+input);
+			}
+			else {
+				temp.put(tempValue, true);
+			}
+			
+			
+		}
 		
 		
 		return input;
+		
+		
 	}
 	
 	
